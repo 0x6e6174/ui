2a8070cc08782f3a464c8b49ae08e0d502d6e5ae
gnat <natalieconnolly621@gmail.com>
2024-11-30 20:13:14 -0800

    make mpDris2 automatically start

--- init.lua
+++ init.lua
@@ -1,15 +1,19 @@
 require("lua.globals")
 
-local bar <const> = require('lua.widgets.bar')
+local bar = require('lua.widgets.bar')
+local notifications = require('lua.widgets.notifications')
 
-local scss <const> = "./style/style.scss"
-local css <const> = "./style/style.css"
+local scss = "./style/style.scss"
+local css = "./style/style.css"
 
 exec(string.format("sassc %s %s", scss, css))
+
+exec('pkill -f /usr/bin/mpDris2; mpDris2')
 
 App:start({
   css = css,
   main = function()
     bar:show_all()
+    notifications:show_all()
   end,
 })
